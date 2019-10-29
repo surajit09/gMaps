e6376d938cdd06b79d85cb4b7688d35c7c97f6c1
Surajit Mukherjee <surajit@surajitspace.onmicrosoft.com>
2019-10-29 20:44:15 +0000

    Added code for google maps

---
+++ src/Company.ts
@@ -0,0 +1,25 @@
+import faker from "faker";
+
+export class Company {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+
+  markerContent(): string {
+    return `Name : ${this.companyName}
+    catchPhrase:${this.catchPhrase}
+    `;
+  }
+}
